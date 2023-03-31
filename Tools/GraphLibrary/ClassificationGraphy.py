7eab6c3e44aaa6e691c8465767c0ce2e27cbf076
Ted <ckyuhyun@gmail.com>
2023-03-30 23:10:16 -0400

    support multipl drawing in sequence.

--- Tools/GraphLibrary/ClassificationGraphy.py
+++ Tools/GraphLibrary/ClassificationGraphy.py
@@ -1,3 +1,5 @@
+import random
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,35 +16,65 @@ class ClassificationGraphy:
         self._x_label = x_label
         self._y_label = y_label
         self.coordinates = {}
+        self.base_node = {}
+        self.init()
 
-        self.generate_coordinate()
+    def init(self):
+        self.generate_relation_coordinate()
+        #self.base_node = self.coordinates.keys()[0]
 
-    def Draw(self):
-        x_coordinate = [x for x, y in list(self.coordinates.values())]
-        y_coordinate = [y for x, y in list(self.coordinates.values())]
+    def Run(self):
+        self.generate_relation_coordinate()
+        keys = []
+        for _key in self.coordinates.keys():
+            keys.append(_key)
 
-        plt.scatter(x_coordinate, y_coordinate,  alpha=0.3)
-        plt.text(x_coordinate[0], y_coordinate[0],"Base Action")
+        # first start
+        _key = random.choices(keys)[0]
+        near_nodes = self._data.loc[self._data[self._data.columns[0]] == _key, self._data.columns[1]]
+        coordinate = [self.coordinates[node_key] for node_key in near_nodes.values]
+        center_x, center_y = coordinate[0]
 
-        center_x, center_y = x_coordinate[0], y_coordinate[0]
-        for x, y in zip(x_coordinate[1:], y_coordinate[1:]):
-            plt.plot(np.array([center_x, x]), np.array([center_y, y]))
+        for i in range(0, 10):
+            self.draw(center_x,
+                      center_y,
+                      np.array(coordinate)[1:, :1],
+                      np.array(coordinate)[1:, 1:2],
+                      self._x_label,
+                      self._y_label)
+
+            _key = random.choice(list(near_nodes.values))
+            coordinate = [self.coordinates[node_key] for node_key in near_nodes.values]
+            random_index = random.randint(0, len(near_nodes.values))
+            center_x , center_y = coordinate[random_index]
+
+            print('')
 
 
-        plt.xlabel(self._x_label)
-        plt.ylabel(self._y_label)
+
+    def draw(self, center_x, center_y, x_coorindates, y_coordinates, x_label, y_label):
+        plt.clf()
+        plt.scatter(x_coorindates, y_coordinates,  alpha=0.3)
+        plt.text(center_x, center_y, "Base Action")
+
+        for x, y in zip(x_coorindates, y_coordinates):
+            plt.plot(np.array([center_x, x[0]]), np.array([center_y, y[0]]))
+
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
         plt.axis('off')
         plt.show()
 
     '''
     Generate coordinates for each action 
     '''
-    def generate_coordinate(self):
+    def generate_relation_coordinate(self):
         _data = np.concatenate((self._data[self._data.columns[0]].values,self._data[self._data.columns[1]].values))
         for _d in _data:
             if _d not in self.coordinates.keys():
                 coordinates = np.array([np.random.randint(0,self.axis_width), np.random.randint(0,self.axis_height)])
                 self.coordinates[_d] = coordinates
+
 
 
 
--- tensorflow/Runner.py
+++ tensorflow/Runner.py
@@ -3,11 +3,11 @@ from Tools.GraphLibrary import ClassificationGraphy
 
 data = pd.read_csv("../action_distance.csv")
 
-_data = data.loc[:, ["ActionId", "Next", "distance", "rating"]][:5]
+_data = data.loc[:, ["ActionId", "Next", "distance", "rating"]]
 
 
 print(_data)
 class_graphy = ClassificationGraphy.ClassificationGraphy(_data, "Current Action", "Next Action")
-class_graphy.Draw()
+class_graphy.Run()
 
 
