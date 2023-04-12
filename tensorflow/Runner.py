6a048c99d3fc9b3cec189c1422ce53bbd31b609c
ckyuhyun@gmail.com <tcho@plexusintl.com>
2023-04-12 13:55:03 -0400

    completing labelling and feature scaling

--- tensorflow/Runner.py
+++ tensorflow/Runner.py
@@ -11,23 +11,39 @@ data.columns = [c.strip() for c in data.columns]
 
 _data = data.loc[:, ["ActionId", "Next", "distance", "rating"]]
 
-#_data['Next'] = _data['Next'].astype(int)
-df = pd.get_dummies(data=_data, columns=['Next'])
-df_next = df.loc[:, [4]]
 
+# collect unique action Ids
+unique_action_id_collections = []
+action_id_collections = list(_data['ActionId'].values) + list(_data['Next'].values)
+for a in action_id_collections:
+    if a not in unique_action_id_collections:
+        unique_action_id_collections.append(a)
 
-
-'''
-df_boolean_data = df.loc[:, ~df.columns.isin(["ActionId", "Next", "distance", "rating"])]
+# Feature label
 le = LabelEncoder()
-_data['Next_Category'] = le.fit_transform(_data['Next'])
+df_label = pd.DataFrame(pd.Series(unique_action_id_collections), columns=['Action_Id_Category'])
+df_label['Action_Id_Category_label'] = le.fit_transform(df_label['Action_Id_Category'])
 
-print(_data.head())
+for x, y in zip(df_label['Action_Id_Category'].values, df_label['Action_Id_Category_label'].values):
+    _data['ActionId'] = _data['ActionId'].replace([x],y)
+    _data['Next'] = _data['Next'].replace([x], y)
 
-std = StandardScaler()
-scaled = std.fit(_data[['Next_Category']].to_numpy())
-scaled = pd.DataFrame(scaled, columns='Next_Category')
-'''
+# turn boolean value into numerical value on Next columns
+
+# Generate dummy columns for the Next
+df = pd.get_dummies(data=_data, columns=['Next'])
+df_next_columns = [c for c in df.columns if "Next" in c]
+
+for column in df_next_columns:
+    df[column] = df[column].astype(int)
+
+
+
+
+# std = StandardScaler()
+# scaled = std.fit(_data[['Next_Category']].to_numpy())
+# scaled = pd.DataFrame(scaled, columns='Next_Category')
+
 
 #_data.drop(columns=['Next_Category'], axis=1, inplace=True)
 #_data = _data.merge(scaled, left_index=True, right_index=True, how = "left")
